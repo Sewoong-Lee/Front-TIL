b7d676570f876530ba4bd238747b79528aeeff26
Sewoong-Lee <81853455+Sewoong-Lee@users.noreply.github.com>
2021-09-19 01:13:58 +0900

    20210919

---
+++ vus_js/test1/src/components/KossieCoder.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <button @click="updated">클릭</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name : '코지코더 유튜브',
+        }
+    },
+    methods: {
+        updated(){
+            this.name = '구독과 조아요~';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vus_js/test1/src/views/Home.vue
@@ -0,0 +1,31 @@
+<!-- html코드 -->
+<template> 
+  <div>
+    <h1>This is Home page</h1>
+    <p>{{name}}</p>
+    <KossieCoder/>
+  </div>
+</template>
+
+<!-- 데이터, 메소드, 와치 등등 -->
+<script>
+import KossieCoder from '@/components/KossieCoder.vue';
+
+export default {
+  components: {
+    KossieCoder
+  },
+  data(){
+    return{
+      name : '이름이다'
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* scoped: 이 파일에만 스타일이 적용되게 제한 */
+h1 {
+  color: red;
+}
+</style>
